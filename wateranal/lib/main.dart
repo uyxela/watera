e196a353efbaf24e5cc65925bf8868244104ecf6
Aryan Misra <aryanmisra4@gmail.com>
2020-06-07 04:38:31 -0400

    dashboard, resources

--- wateranal/lib/main.dart
+++ wateranal/lib/main.dart
@@ -50,6 +50,11 @@ class Menu extends StatelessWidget {
       body: Column (
         children: <Widget>[
           Card(
+            margin: EdgeInsets.all(10),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 5,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -157,13 +162,34 @@ class Dashboard extends StatelessWidget {
 
         ],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
-        ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Card(
+                margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10),
+                child: Column(
+                  children: <Widget>[
+                    const ListTile(
+//                  leading: Icon(Icons.album),
+                      title: Text('Average Temperature'),
+                      subtitle: Text('in Fahrenheit'),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 3, bottom: 10),
+                        height: 200.0,
+                        child: StackedAreaLineChart.withSampleData()
+                    ),
+                  ],
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 5,
+              ),
+            ],
+          ),
+        ],
       ),
       drawer: DrawerTab()
     );
@@ -193,13 +219,63 @@ class Resources extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
-        ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 5,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        child: Icon(Icons.album),
+                        height: 30,
+                      )
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                          margin: const EdgeInsets.only(top: 15),
+                          alignment: Alignment.centerLeft,
+                          height: 50,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Water Contamination", style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text('txt')
+                            ],
+                          )
+                      ),
+                    ),
+                    Expanded(
+                        child: Container(
+                          child: Icon(Icons.share),
+                          height: 30,
+                        )
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 250,
+                  width: 500,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/placeholder.jpeg"),
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          )
+
+        ],
       ),
       drawer: DrawerTab()
     );
@@ -345,4 +421,85 @@ class DrawerTab extends StatelessWidget {
       ),
     );
   }
+}
+
+class StackedAreaLineChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  StackedAreaLineChart(this.seriesList, {this.animate});
+
+  /// Creates a [LineChart] with sample data and no transition.
+  factory StackedAreaLineChart.withSampleData() {
+    return new StackedAreaLineChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: true,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.LineChart(seriesList,
+        defaultRenderer:
+        new charts.LineRendererConfig(includeArea: true, stacked: true),
+        animate: animate);
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
+    final myFakeDesktopData = [
+      new LinearSales(0, 5),
+      new LinearSales(1, 25),
+      new LinearSales(2, 100),
+      new LinearSales(3, 75),
+    ];
+
+    var myFakeTabletData = [
+      new LinearSales(0, 10),
+      new LinearSales(1, 50),
+      new LinearSales(2, 200),
+      new LinearSales(3, 150),
+    ];
+
+    var myFakeMobileData = [
+      new LinearSales(0, 15),
+      new LinearSales(1, 75),
+      new LinearSales(2, 300),
+      new LinearSales(3, 225),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Desktop',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: myFakeDesktopData,
+      ),
+      new charts.Series<LinearSales, int>(
+        id: 'Tablet',
+        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: myFakeTabletData,
+      ),
+      new charts.Series<LinearSales, int>(
+        id: 'Mobile',
+        colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: myFakeMobileData,
+      ),
+    ];
+  }
+}
+
+/// Sample linear data type.
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
 }
